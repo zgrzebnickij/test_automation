044e8bd7ea2bc6566fcdbc05802bf3df7cf14033
jakub <jakub.zgrzeb@gmail.com>
2021-05-12 18:04:19 +0200

    check numbers from 2 to 10

--- factorization.py
+++ factorization.py
@@ -1,2 +1,12 @@
-def prime_factors():
-    pass
+def prime_factors(number):
+    i = 2
+    factors = []
+    while i * i <= number:
+        if number % i:
+            i += 1
+        else:
+            number //= i
+            factors.append(i)
+    if number > 1:
+        factors.append(number)
+    return list(set(factors))
--- test_factorization.py
+++ test_factorization.py
@@ -1,3 +1,5 @@
+from factorization import prime_factors
+
 
 def test_import_function():
     try:
@@ -5,3 +7,19 @@ def test_import_function():
         assert callable(prime_factors), "prime_factors not callable" 
     except ImportError as error: 
         assert False, error
+
+def test_first_10_numbers():
+    cases = {
+        2: [2],
+        3: [3],
+        4: [2],
+        5: [5],
+        6: [2,3],
+        7: [7],
+        8: [2],
+        9: [3],
+        10: [2, 5],
+    }
+    for case, should_return in cases.items():
+        result = prime_factors(case)
+        assert result == should_return, f'prime_factors for {case} should return {should_return}, but get {result}'
