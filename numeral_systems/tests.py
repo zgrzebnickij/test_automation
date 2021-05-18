977e730acc9513b5b06028f9fbe1d538ffb78971
jakub <jakub.zgrzeb@gmail.com>
2021-05-18 20:41:49 +0200

    add test for checking input type

--- numeral_systems/tests.py
+++ numeral_systems/tests.py
@@ -21,3 +21,24 @@ class test_roman_to_decimal(unittest.TestCase):
         for roman, decimal in (('V', 5),('IV', 4), ('VIII', 8)):
             result =  roman_to_decimal(roman)
             assert result == decimal, f'Fail roman_to_decimal for {roman} should be {decimal}, but got {result}'
+
+    def test_to_100_numbers(self):
+        for roman, decimal in (('X', 10),('IX', 9), ('XVIII', 18), ('XXXV', 35), ('XCIX', 99)):
+            result =  roman_to_decimal(roman)
+            assert result == decimal, f'Fail roman_to_decimal for {roman} should be {decimal}, but got {result}'
+
+    def test_to_1000_numbers(self):
+        for roman, decimal in (('CXLVII', 147),('CCCXIII', 313), ('CLXXXV', 185), ('DLXXXV', 585), ('DCCLIV', 754)):
+            result =  roman_to_decimal(roman)
+            assert result == decimal, f'Fail roman_to_decimal for {roman} should be {decimal}, but got {result}'
+
+    def test_invalid_types(self):
+        'Test wrong types of input'
+        for invalid in ('', 234, True):
+            try:
+                result =  roman_to_decimal(invalid)
+                assert False, 'Should return Value error'
+            except ValueError as error: 
+                pass
+            except Exception as error:
+                assert False, f'Wrong error expected ValueError, but got {error}'
