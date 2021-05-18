5b390fc048100e492bd7cc6ff006c7a24fe03e27
jakub <jakub.zgrzeb@gmail.com>
2021-05-18 20:47:11 +0200

    add type checking

--- numeral_systems/numeralSystems.py
+++ numeral_systems/numeralSystems.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 ROMAN_TO_DECIMAL_MAP = {
     'I': 1,
     'V': 5,
@@ -8,23 +10,34 @@ ROMAN_TO_DECIMAL_MAP = {
     'M': 1000
 }
 
+WHEN_BEFORE = { 
+    'I': ('V', 'X'),
+    'X': ('L', 'C'),
+    'C': ('D', 'M'),
+}
 
+# I can only be placed before V and X.
+# X can only be placed before L and C.
+# C can only be placed before D and M.
 
 # Numerals must be arranged in descending order of size.
 # M, C, and X cannot be equalled or exceeded by smaller denominations.
 # D, L, and V can each only appear once.
 
 # Only one I, X, and C can be used as the leading numeral in part of a subtractive pair.
-WHEN_BEFORE = { 
-    'I': ('V', 'X'),
-    'X': ('L', 'C'),
-    'C': ('D', 'M'),
-}
-# I can only be placed before V and X.
-# X can only be placed before L and C.
-# C can only be placed before D and M.
 
+def validation(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        roman = args[0]
+        if type(roman) != str:
+            raise ValueError('Expected string')
+        if roman == '':
+            raise ValueError('String canot be empty')
+        return func(*args, **kwargs)
+    return inner
 
+@validation
 def roman_to_decimal(roman):
     before = ''
     result = 0
