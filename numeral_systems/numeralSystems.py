34040bcc1e50670bcbabfb03e7f900e836af8532
jakub <jakub.zgrzeb@gmail.com>
2021-05-18 20:22:10 +0200

    add working for first 3 numbers

--- numeral_systems/numeralSystems.py
+++ numeral_systems/numeralSystems.py
@@ -1,2 +1,27 @@
-def roman_to_decimal():
-    pass
+ROMAN_TO_DECIMAL_MAP = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
+}
+
+# Numerals must be arranged in descending order of size.
+# M, C, and X cannot be equalled or exceeded by smaller denominations.
+# D, L, and V can each only appear once.
+
+# Only one I, X, and C can be used as the leading numeral in part of a subtractive pair.
+# I can only be placed before V and X.
+# X can only be placed before L and C.
+# C can only be placed before D and M.
+
+
+def roman_to_decimal(roman):
+    before = ''
+    result = 0
+    for current in roman:
+        result += ROMAN_TO_DECIMAL_MAP[current]
+        # before = current
+    return result
